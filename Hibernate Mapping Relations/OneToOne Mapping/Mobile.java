3f22f34423e5dc3bc8b39b2717380d9c9f885393
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-24 16:35:49 +0530

    Update Mobile.java

--- Hibernate Mapping Relations/OneToOne Mapping/Mobile.java
+++ Hibernate Mapping Relations/OneToOne Mapping/Mobile.java
@@ -1,1 +1,34 @@
+package com.ebaad.HibernateMappingRelations;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity // An entity defines a separate table in the database
+public class Mobile {
+	
+	@Id // As mid is the primary key in Mobile Table so in Person Table we will have mid as the foreign key
+	private int mid;
+	private String mname;
+	
+	@Override
+	public String toString() {
+		return "Mobile [mid=" + mid + ", mname=" + mname + "]";
+	}
+	
+	public int getMid() {
+		return mid;
+	}
+	
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	
+	public String getMname() {
+		return mname;
+	}
+	
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+}
 
