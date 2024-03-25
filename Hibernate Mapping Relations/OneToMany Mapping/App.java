97b790fd66425fbd6a95b6f2bcd9d5d3bf388b3b
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-25 14:36:13 +0530

    Update App.java

--- Hibernate Mapping Relations/OneToMany Mapping/App.java
+++ Hibernate Mapping Relations/OneToMany Mapping/App.java
@@ -1,1 +1,40 @@
+package com.ebaad.HibernateMappingRelations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class App {
+    public static void main( String[] args ) {
+    	
+    	// Creating an instance of Mobile Class
+    	Mobile mobile = new Mobile();
+    	mobile.setMid(101);
+    	mobile.setMname("Samsung");
+    	
+    	
+    	// Creating an instance of Person Class
+    	Person person = new Person();
+    	person.setRollno(1);
+    	person.setName("Ebaad");
+    	person.setCourse("BCA");	
+    	person.getMobile().add(mobile); // relation between person and mobile(One person multiple mobiles)
+      	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(Person.class).addAnnotatedClass(Mobile.class);
+    	ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+    	SessionFactory sf = con.buildSessionFactory(reg);
+    	Session session = sf.openSession();
+    	
+    	session.beginTransaction();
+    	
+    	session.save(mobile);
+    	session.save(person);
+    	
+    	session.getTransaction().commit();
+    	
+    
+    }
+}
 
