cdf7b65ad19adcef5da006f314c0d969a5f74f18
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-25 14:54:23 +0530

    Update Person.java

--- Hibernate Mapping Relations/ManyToOne Mapping/Person.java
+++ Hibernate Mapping Relations/ManyToOne Mapping/Person.java
@@ -1,1 +1,44 @@
+package com.ebaad.HibernateMappingRelations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity // An entity defines a separate table in the database
+public class Person {
+	
+	@Id // rollno is the primary key
+	private int rollno;
+	private String name;
+	private String course;
+
+	@Override
+	public String toString() {
+		return "Person [rollno=" + rollno + ", name=" + name + ", course=" + course + "]";
+	}
+
+	public int getRollno() {
+		return rollno;
+	}	
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}	
+	public String getName() {
+		return name;
+	}	
+	public void setName(String name) {
+		this.name = name;
+	}	
+	public String getCourse() {
+		return course;
+	}	
+	public void setCourse(String course) {
+		this.course = course;
+	}
+}
 
