17c1c1b6087ff9cf5df9e877aea8a8faaebb4a73
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-25 16:17:55 +0530

    Update Mobile.java

--- Hibernate Mapping Relations/ManyToMany Mapping/Mobile.java
+++ Hibernate Mapping Relations/ManyToMany Mapping/Mobile.java
@@ -1,1 +1,46 @@
+package com.ebaad.HibernateMappingRelations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity // An entity defines a separate table in the database
+public class Mobile {
+	
+	@Id // As mid is the primary key in Mobile Table so in Person Table we will have mid as the foreign key
+	private int mid;
+	private String mname;
+	
+	// ManyToMany relationship where multiple persons have mutiple mobiles
+	@ManyToMany
+	private List<Person> person = new ArrayList<>();
+	public List<Person> getPerson() {
+		return person;
+	}
+	public void setPerson(List<Person> person) {
+		this.person = person;
+	}
+
+	@Override
+	public String toString() {
+		return "Mobile [mid=" + mid + ", mname=" + mname + "]";
+	}
+  
+	public int getMid() {
+		return mid;
+	}	
+	public void setMid(int mid) {
+		this.mid = mid;
+	}	
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+}
 
