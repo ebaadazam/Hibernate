22bbcc1a140b5044996c5abf1f47c7c8977b1f05
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-26 20:05:20 +0530

    Update Humans.java

--- Hibernate Caching/Second Level Cache/Humans.java
+++ Hibernate Caching/Second Level Cache/Humans.java
@@ -1,9 +1,17 @@
-package com.ebaad.HibernateCaching;
+package com.ebaad.HibernateCaching2;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+
 
 @Entity // An entity defines a separate table in the database
+@Cacheable // This entity allows the class to get cached
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)  // Specify the strategy of Cache
 public class Humans {
 	
 	@Id // id is the primary key
