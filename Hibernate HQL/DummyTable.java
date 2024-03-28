1af419d23f8b3b5423d8f59af33188962295441d
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-28 15:02:04 +0530

    Update DummyTable.java

--- Hibernate HQL/DummyTable.java
+++ Hibernate HQL/DummyTable.java
@@ -1,1 +1,37 @@
+package com.ebaad.HibernateHQL;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DummyTable {
+	@Id
+	private int id;
+	private String name;
+	private int score;
+	
+	@Override
+	public String toString() {
+		return "Id=" + id + ", Name=" + name + ", Score=" + score;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
 
